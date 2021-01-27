8471b3a654fdaecc2a167ded4125cb958df36159
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-27 20:43:45 -0300

    dados capturados na API passados pro front e render

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 require('dotenv').config();
 
-//config de server
-const express = require('express');
-const path = require('path');
 
-const app = express();
-
-app.set('view engine', 'ejs');
-
-
-app.get('/', function(req, res){ 
-    res.render('index');
-});
-
-
-app.listen(3000, function(){
-    console.log("Rodando server na porta 3000");
-});
 
 //config de server fim
 
 
 const axios = require('axios');
+const { urlencoded } = require('express');
 
 const params = {
   access_key: process.env.KEY,
@@ -42,8 +27,34 @@ axios.get(url)
     const apiResponse = response.data;
     //console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
     console.log(apiResponse.location.region);
+
+    //config de server
+    const express = require('express');
+    const path = require('path');
+    const bodyParser = require('body-parser')
+
+    const app = express();
+
+    app.set('view engine', 'ejs');
+    app.use(express.static('/public'));
+
+    app.use(bodyParser.urlencoded({extended : true}));
+
+
+
+    app.get('/', function(req, res){ 
+        res.render('index', {apiResponse : apiResponse}); //passando os dados capturados na API pro front
+    });
+
+
+    app.listen(3000, function(){
+        console.log("Rodando server na porta 3000");
+});
+    
   }).catch(error => {
     console.log(error);
-  });
+});
+
+
 
 
