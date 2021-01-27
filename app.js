6381c9f7fe5f27f14c7399a3d702b0b8fc1827bf
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-26 23:27:04 -0300

    ainda não funciona

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 require('dotenv').config();
+
+//config de server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){ 
+    res.render('/views/index');
+});
+
+
+app.listen(3000, function(){
+    console.log("Rodando server na porta 3000");
+});
+
+//config de server fim
+
+/*
 const axios = require('axios');
-//require('process');
 
 const params = {
   access_key: process.env.KEY,
@@ -11,6 +32,7 @@ const params = {
 console.log(params.access_key);
 console.log(params.query);
 
+
 const url = `http://api.weatherstack.com/current?access_key=${params.access_key}&query=${params.query}`;
 
 
@@ -18,8 +40,11 @@ const url = `http://api.weatherstack.com/current?access_key=${params.access_key}
 axios.get(url)
   .then(response => {
     const apiResponse = response.data;
-    //console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);*/
+    //console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
     console.log(apiResponse.location.region);
   }).catch(error => {
     console.log(error);
   });
+
+
+*/
