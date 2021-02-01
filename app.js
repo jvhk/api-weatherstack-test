10a424b80087be0c66dc061ccdd5e8526707100e
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-31 21:37:26 -0300

    remoção de codigo desnecessario

--- app.js
+++ app.js
@@ -1,67 +1,74 @@
 require('dotenv').config();
 
-
-//config de server
-
-//config de server fim
-
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const axios = require('axios');
-const { urlencoded } = require('express');
-
-const params = {
-  access_key: process.env.KEY,
-  query: 'Brazil,Bambui'
-}
 
 
-//console.log(params.access_key);
-//console.log(params.query);
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('/public'));
+
+app.use(bodyParser.urlencoded({extended : true}));
 
 
-const url = `http://api.weatherstack.com/current?access_key=${params.access_key}&query=${params.query}`;
 
+//const url = `http://api.weatherstack.com/current?access_key=${params.access_key}&query=${params.query}`;
+
+app.get('/', function (req,res) { 
+  axios.post
+  let cidade = req.body.cidade;
+  let pais = req.body.pais;
+  console.log("cidade pelo body parser: " + cidade);
+  console.log("cidade pelo body parser: " + pais);
+
+  const params = {
+    access_key: process.env.KEY,
+    query: `${cidade},${pais}`
+    //query: 'Brazil,Bambui'
+  }
+
+  const url = `http://api.weatherstack.com/current?access_key=${params.access_key}&query=${params.query}`;
+
+  axios.get(url)
+    .then(response =>{
+      const apiResponse = response.data;
+      //console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+      console.log(apiResponse.location.region);
+      
+      app.get('/', function(req, res){ 
+        res.render('index', {apiResponse : apiResponse}); //passando os dados capturados na API pro front
+      });
+      
+    }).catch(error => {
+      console.log(error);
+  });
+  
+})
+
+app.listen(3000, function(){
+  console.log("Rodando server na porta 3000");
+});
 
 //axios.get('https://api.weatherstack.com/current', {params})
+
+/*
 axios.get(url)
   .then(response => {
     const apiResponse = response.data;
     //console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
     console.log(apiResponse.location.region);
 
-    //config de server
-    
-    const express = require('express');
-    const path = require('path');
-    const bodyParser = require('body-parser')
-
-    const app = express();
-
-    app.set('view engine', 'ejs');
-    app.use(express.static('/public'));
-
-    app.use(bodyParser.urlencoded({extended : true}));
-
-
-
-    app.get('/', function(req, res){ 
-        res.render('index', {apiResponse : apiResponse}); //passando os dados capturados na API pro front
-        
-        app.post('/', function (req,res) { 
-            let cidade = req.body.cidade;
-            console.log("cidade pelo body parser: " + cidade);
-        })
-    });
-
-
-    app.listen(3000, function(){
-        console.log("Rodando server na porta 3000");
-});
     
   }).catch(error => {
     console.log(error);
 });
+*/
 
-
-
+/*
+app.get('/', function(req, res){ 
+        res.render('index', {apiResponse : apiResponse}); //passando os dados capturados na API pro front
+    });
+*/
 
